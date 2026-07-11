c242a74b6fc1f8abae1a74947b5c1d8badfd645b
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-07-11 19:04:04 +0530

    Implemented Dialog box to confirm delete Category action

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:todo_list/data/categories.dart";
 import "package:todo_list/widgets/action_button.dart";
+import "package:todo_list/widgets/primary_button.dart";
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({
@@ -41,7 +42,53 @@ class CategoryCard extends StatelessWidget {
               ActionButton(
                 icon: Icon(Icons.delete_forever_rounded),
                 onPressed: () {
-                  CategoryHandler.removeCategory(id: currentCategory.id);
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Row(
+                          spacing: 10,
+                          children: [
+                            Icon(
+                              Icons.warning_rounded,
+                              size: Theme.of(
+                                context,
+                              ).textTheme.headlineLarge?.fontSize,
+                            ),
+                            Text(
+                              "Delete Category?",
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            spacing: 10,
+                            children: [
+                              PrimaryButton(
+                                text: "Cancel",
+                                icon: Icon(Icons.cancel_rounded),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              PrimaryButton(
+                                text: "Confirm",
+                                icon: Icon(Icons.check_circle_rounded),
+                                onPressed: () {
+                                  CategoryHandler.removeCategory(
+                                    id: currentCategory.id,
+                                  );
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
               ),
             ],
