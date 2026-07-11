c384a8a7dbe5486c1c60ab39a170f64a95e80457
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-07-11 11:06:28 +0530

    Modified background color in ContentColumn to change with Brightness

--- lib/widgets/content_column.dart
+++ lib/widgets/content_column.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/data/settings.dart';
 
 class ContentColumn extends StatelessWidget {
   const ContentColumn({super.key, required this.children});
@@ -9,7 +10,9 @@ class ContentColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.inversePrimary.withAlpha(25),
+        color: Theme.of(context).colorScheme.inversePrimary.withAlpha(
+          Settings.useDarkBrightness.value ? 25 : 75,
+        ),
         border: Border.all(
           color: Theme.of(context).colorScheme.inversePrimary,
           width: 3,
